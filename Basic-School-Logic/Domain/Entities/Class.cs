50550b537a694bd34bbe87d50abc4b256e07b776
SamZuckerberg <samzuckerberg.jar@gmail.com>
2020-11-05 11:40:59 -0300

    Created basic Clean Architecture

---
+++ Basic-School-Logic/Domain/Entities/Class.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Basic_School_Logic.Domain.Entities
+{
+    public class Person
+    {
+    }
+}
